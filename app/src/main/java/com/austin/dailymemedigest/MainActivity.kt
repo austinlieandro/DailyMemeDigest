fa13d464813e3dba64c8bd7c308f820383c2da8a
anellaaaa <anellanevanyatjokro@gmail.com>
2022-12-15 15:18:49 +0700

    drawer dah bisa

--- app/src/main/java/com/austin/dailymemedigest/MainActivity.kt
+++ app/src/main/java/com/austin/dailymemedigest/MainActivity.kt
@@ -3,12 +3,21 @@ package com.austin.dailymemedigest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.drawer_layout)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="Daily Meme Digest"
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        var drawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.app_name,
+                R.string.app_name)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
     }
 }
