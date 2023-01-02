7b3fa436fef8c9e09c68ebdbd9817da6ea8aca30
Yongky <85381312+Envyy92@users.noreply.github.com>
2023-01-02 21:42:41 +0700

    hubungin login activity sama register

--- app/src/main/java/com/austin/dailymemedigest/LoginActivity.kt
+++ app/src/main/java/com/austin/dailymemedigest/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.austin.dailymemedigest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this,RegisterActivity:: class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
